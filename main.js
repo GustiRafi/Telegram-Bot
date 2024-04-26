938fab6c2a3ab17696d0c81be5b3eff75da372bf
GustiRafi <gustirafi49@gmail.com>
2024-04-26 15:06:19 +0700

    feat: tanya gempa

--- main.js
+++ main.js
@@ -8,7 +8,32 @@ const options = {
 
 const bot = new TelegramBot(token,options)
 
-bot.on('message', (callback) => { 
+
+// regex
+const prefix = "."
+
+const gempa = new RegExp(`^${prefix}gempa$`);
+
+
+bot.onText(gempa,async(callback) => {
+    const BMKG_ENDPOINT = "https://data.bmkg.go.id/DataMKG/TEWS/";
+
+    const fetchCall = await fetch(`${BMKG_ENDPOINT}autogempa.json`)
+    const {Infogempa: { gempa: { Tanggal,Jam,Magnitude,Kedalaman,Wilayah,Potensi,Shakemap,Coordinates } } } = await fetchCall.json()
+
+    const image = BMKG_ENDPOINT + Shakemap;
+    const latitude = Coordinates.split(',')[0];
+    const longitude = Coordinates.split(',')[1];
+    const result = `
+    Waktu : ${Tanggal}, ${Jam}
+Wilayah: ${Wilayah}
+Kedalaman: ${Kedalaman}
+Magnitude: ${Magnitude}
+Potensi: ${Potensi}
+    `
     const chatFrom = callback.from.id;
-    bot.sendMessage(chatFrom,"Silahkan....")
- })
+    bot.sendLocation(chatFrom,latitude,longitude)
+    bot.sendPhoto(chatFrom,image,{
+        caption: result
+    })
+})
